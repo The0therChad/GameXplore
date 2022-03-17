a651e1949e8e4d5b5f0b42836042e7154320ca42
The0therChad <callmewheelz@gmail.com>
2022-03-17 16:09:28 -0700

    Video search foundation implemented

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ from settings import ID, TOKEN
 
 wrapper = IGDBWrapper(ID, TOKEN)
 
-def query(endpoint: str, fields: str = None, where: str = None) -> list:
+def query(endpoint: str, fields: str, where: str) -> list:
     """Query the API with desired parameters"""
     request = wrapper.api_request(
         f"{endpoint}, fields {fields}; where {where}"
@@ -12,10 +12,18 @@ def query(endpoint: str, fields: str = None, where: str = None) -> list:
     result = json.loads(request)
     return result
 
-def query_search(endpoint: str, fields: str = "name", search: str = None) -> list:
+def query_search(endpoint: str, search: str, fields: str = "name") -> list:
     """Search the API with desired parameters"""
     request = wrapper.api_request(
         endpoint, f'fields {fields}; search "{search}";'
+    )
+    result = json.loads(request)
+    return result
+
+def query_video(where: str) -> list:
+    """Get a selection of Youtube links based on selected game"""
+    request = wrapper.api_request(
+        'game_videos', f'fields video_id; where game={where};'
     )
     result = json.loads(request)
     return result
--- commands.py
+++ commands.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from api import query_search
+from api import query_search, query_video
 
 class QueryCog(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
@@ -23,6 +23,22 @@ class QueryCog(commands.Cog):
             msg = msg + f"{place}. {name}\n"
             place += 1
         await ctx.send(msg)
+
+    @commands.command(name="video")
+    @commands.cooldown(rate=1, per=2)
+    async def video(self, ctx: commands.Context, *, text: str):
+        """Get a selection of Youtube links based on selected game"""
+        search_results = query_search("games", search=text)
+        game_id = search_results[0]["id"]
+        query_results = query_video(where=game_id)
+        ids = []
+        for item in query_results:
+            ids.append(item["video_id"])
+        if ids == []:
+            await ctx.send("There are no videos for that game. :confused:")
+        for id in ids:
+            msg = f"https://www.youtube.com/watch?v={id}"
+            await ctx.send(msg)
 
 def setup(bot: commands.Bot):
     bot.add_cog(QueryCog(bot))
